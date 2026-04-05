a60c045aee789c38984250b56e19b1fa683d153a
ixHaMaD <7xD.1422@gmail.com>
2026-04-05 14:39:05 +0300

    added the base.html file and rendered it in index.html

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.home_page, name='home_page'), #in views.py we must add the index function
     path('2', views.second_page, name='second_page'),
     path('book/<val>', views.third_page, name='third_page'),
+    path('forth',views.forth_page, name='forth_page')
 ]
 
 
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+name = 'Hamad Mohammed'
 
 def home_page(request):
     #renders the appropriate template for this request
-    return render(request, 'bookmodule/index.html') #the index.html file must be in the templates
+    return render(request, 'bookmodule/index.html', {'var1':name}) #the index.html file must be in the templates
 
 def second_page(request):
 
@@ -11,3 +11,6 @@ def second_page(request):
 
 def third_page(request, val):
     return render(request, 'bookmodule/index3.html')
+
+def forth_page(request):
+    return render(request, 'bookmodule/index4.html')
